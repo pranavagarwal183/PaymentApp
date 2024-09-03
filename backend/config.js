dd2bc9de377eeb799023f5efce7adc2648e89be1
pranavagarwal183 <agarwalpranav177@gmail.com>
2024-09-03 22:34:54 +0530

    Commit

---
+++ backend/config.js
@@ -0,0 +1,2 @@
+const JWT_SECRET="admin123";
+module.exports=JWT_SECRETS
--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,10 @@
 const express = require("express");
+const mainRouter=require("./routes/index.js")
+const cors=require("cors");
+const app=express();
 
+app.use(cors());
+app.use(express.json());
+app.use("/api/vi",mainRouter);
 
+app.listen(3000);
