c3aadb5f1b1bbc1638334865d8fa8d1d7fedff96
pranavagarwal183 <agarwalpranav177@gmail.com>
2024-09-04 16:11:04 +0530

    Initial commit

--- backend/config.js
+++ backend/config.js
@@ -1,2 +1,2 @@
 const JWT_SECRET="admin123";
-module.exports=JWT_SECRETS
+module.exports=JWT_SECRET
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,8 @@
 const express=require("express");
 const userRouter =require("./user");
+const accountRouter = require("./account");
 const router=express.Router();
 
-router.get("/user",userRouter)
+router.use("/user", userRouter);
+router.use("/account", accountRouter);
 module.exports =router;
